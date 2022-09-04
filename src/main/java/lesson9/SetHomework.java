c3afdaac5a753914e2beb514f67b183891729503
mikor85 <cormakoff@gmail.com>
2022-09-04 11:23:37 +0300

    Some changed added

--- src/main/java/lesson9/SetHomework.java
+++ src/main/java/lesson9/SetHomework.java
@@ -8,7 +8,6 @@ package lesson9;
 // "wood", "parrot", "cat", "Alan", "Cuba", "Finland", "Axelrod" , "Avangard", "Cuba"]
 // нужно получить список всех стран из списка слов, начинающихся на "A" - т.е., ["Argentina", "Andorra"]
 
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,17 +17,17 @@ public class SetHomework {
         String[] countries = new String[]{"Andorra", "Belize", "Cayman", "France", "Argentina", "Cuba", "Sweden", "Australia"};
         String[] words = new String[]{"Andorra", "Canada", "First", "candy", "Argentina", "wood", "Australia", "parrot", "cat", "Alan", "Cuba", "Finland", "Axelrod", "Avangard", "Cuba"};
 
-        System.out.println(getCountriesStartedAtA(countries, words));
+        System.out.println(getCountriesListStartedAtA(countries, words));
     }
 
-    private static Set<String> getCountriesStartedAtA(String[] countries, String[] words) {
-        Set<String> countriesStartedAtA = new HashSet<>();
+    private static Set<String> getCountriesListStartedAtA(String[] countries, String[] words) {
+        Set<String> countriesResultList = new HashSet<>();
         Set<String> checkSet = new HashSet<>(List.of(countries));
         for (String w : words) {
             if (checkSet.contains(w) && w.charAt(0) == 'A') {
-                countriesStartedAtA.add(w);
+                countriesResultList.add(w);
             }
         }
-        return countriesStartedAtA;
+        return countriesResultList;
     }
 }
