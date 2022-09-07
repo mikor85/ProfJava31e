2942e8e60d5e37b0a60607dc66b6871253daba85
mikor85 <cormakoff@gmail.com>
2022-09-07 22:49:39 +0300

    CharComparator added

---
+++ src/main/java/lesson10/hs/CharComparator.java
@@ -0,0 +1,10 @@
+package lesson10.hs;
+
+import java.util.Comparator;
+
+public class CharComparator implements Comparator<Character> {
+    @Override
+    public int compare(Character o1, Character o2) {
+        return o1 - o2;
+    }
+}
