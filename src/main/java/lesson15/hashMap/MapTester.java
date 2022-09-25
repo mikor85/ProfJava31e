e3245395777abc56386bed4b47265882432b6cb5
mikor85 <cormakoff@gmail.com>
2022-09-25 18:21:25 +0300

    Homework after Lesson15 has been added

--- src/main/java/lesson15/hashMap/MapTester.java
+++ src/main/java/lesson15/hashMap/MapTester.java
@@ -1,37 +1,68 @@
 package lesson15.hashMap;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTester {
     public static void main(String[] args) {
+        Map<String, String> testMap = new HashMap<>();
+
         MyHashMap map = new MyHashMap();
-        map.put("Moldova", "Kishinev");
-        map.put("Hungary", "Budapest");
-        map.put("Latvia", "Riga");
-        map.put("Germany", "Berlin");
-        map.put("Belarus", "Minsk");
-        map.put("Russia", "Moscow");
-        map.put("France", "Paris");
-        map.put("Spain", "Madrid");
-        map.put("Sweden", "Stockholm");
-        map.put("Italy", "Rome");
-        System.out.println(map.size());
-
-        System.out.println(map.get("Moldova"));
-
-        System.out.println(map.contains("Moldova"));
-        System.out.println(map.contains("Germany"));
-        System.out.println(map.contains("Hungary"));
-
-        System.out.println(map.remove("Germany"));
-        System.out.println(map.size());
-        System.out.println(map.contains("Germany"));
-
-        System.out.println(map.remove("Russia"));
-        System.out.println(map.size());
-        System.out.println(map.contains("Russia"));
-
-        System.out.println(map.remove("Spain"));
-        System.out.println(map.size());
-        System.out.println(map.contains("Spain"));
+        map.put("Moldova", "Kishinev");    // source[bucket] = 0  ->  newSource[bucket] = 0
+        System.out.println("Moldova hashCode = " + "Moldova".hashCode());
+        map.put("Hungary", "Budapest");    // source[bucket] = 2  ->  newSource[bucket] = 6
+        System.out.println("Hungary hashCode = " + "Hungary".hashCode());
+        map.put("Latvia", "Riga");         // source[bucket] = 1  ->  newSource[bucket] = 1
+        System.out.println("Latvia hashCode = " + "Latvia".hashCode());
+        System.out.println("Map size = " + map.size());
+        System.out.println("sourceLength = " + map.sourceLength);
+        map.put("Germany", "Berlin");      // source[bucket] = 3  ->  newSource[bucket] = 3
+        System.out.println("Germany hashCode = " + "Germany".hashCode());
+        System.out.println("Map size = " + map.size());
+        System.out.println("sourceLength = " + map.sourceLength);
+        map.put("Belarus", "Minsk");       // source[bucket] = 0  ->  newSource[bucket] = 0
+        System.out.println("Belarus hashCode = " + "Belarus".hashCode());
+        System.out.println("Map size = " + map.size());
+        System.out.println("sourceLength = " + map.sourceLength);
+//        map.put("Russia", "Moscow");     // source[bucket] = 1  ->  newSource[bucket] = 5
+        System.out.println("Russia hashCode = " + "Russia".hashCode());
+//        System.out.println("Map size = " + map.size());
+//        System.out.println("sourceLength = " + map.sourceLength);
+//        map.put("France", "Paris");      // source[bucket] = 3  ->  newSource[bucket] = 3
+        System.out.println("France hashCode = " + "France".hashCode());
+//        System.out.println("Map size = " + map.size());
+//        System.out.println("sourceLength = " + map.sourceLength);
+//        map.put("Spain", "Madrid");      // source[bucket] = 1  ->  newSource[bucket] = 1
+        System.out.println("Spain hashCode = " + "Spain".hashCode());
+//       System.out.println("Map size = " + map.size());
+//        System.out.println("sourceLength = " + map.sourceLength);
+//        map.put("Sweden", "Stockholm");  // source[bucket] = 0  ->  newSource[bucket] = 4
+        System.out.println("Sweden hashCode = " + "Sweden".hashCode());
+//        System.out.println("Map size = " + map.size());
+//        System.out.println("sourceLength = " + map.sourceLength);
+//        map.put("Italy", "Rome");        // source[bucket] = 3  ->  newSource[bucket] = 3
+        System.out.println("Italy hashCode = " + "Italy".hashCode());
+//        System.out.println("Map size = " + map.size());
+//        System.out.println("sourceLength = " + map.sourceLength);
+//
+//        System.out.println(map.get("Moldova"));
+//
+//        System.out.println(map.contains("Moldova"));
+//        System.out.println(map.contains("Germany"));
+//        System.out.println(map.contains("Hungary"));
+//
+//        System.out.println(map.remove("Germany"));
+//        System.out.println(map.size());
+//        System.out.println(map.contains("Germany"));
+//
+//        System.out.println(map.remove("Russia"));
+//        System.out.println(map.size());
+//        System.out.println(map.contains("Russia"));
+//
+//        System.out.println(map.remove("Spain"));
+//        System.out.println(map.size());
+//        System.out.println(map.contains("Spain"));
 
     }
 }
--- src/main/java/lesson15/hashMap/MyHashMap.java
+++ src/main/java/lesson15/hashMap/MyHashMap.java
@@ -2,12 +2,13 @@ package lesson15.hashMap;
 
 public class MyHashMap implements MyMap {
     private int size = 0;                                   // количество пар в контейнере
-    private static final int INITIAL_CAPACITY = 16;         // начальный размер массива
+    private static final int INITIAL_CAPACITY = 4;         // начальный размер массива
     private static final double LOAD_FACTOR = 0.75;         // коэффициент загруженности
     // если size/source.length >= LOAD_FACTOR, то нужно сбалансировать контейнер
     // для равномерного распределения элементов чтобы не было длинных цепочек
 
     private Pair[] source = new Pair[INITIAL_CAPACITY]; // массив для хранения голов цепочек
+    public int sourceLength = source.length;  // удалить после обкатки кода
 
     private static class Pair {
         String key;     // ключ
@@ -23,7 +24,7 @@ public class MyHashMap implements MyMap {
 
     @Override
     public void put(String key, String value) {
-        if (size > LOAD_FACTOR * source.length) {
+        if (size >= LOAD_FACTOR * source.length) {
             resize();
         }
         Pair pair = findPair(key); // поиск пары по ключу
@@ -35,6 +36,7 @@ public class MyHashMap implements MyMap {
         } else {
             pair.value = value;            // такая пара уже есть, поэтому меняем ее значение
         }
+        System.out.println("<" + key + " " + value + "> - has been added to the Map");
     }
 
     // по ключу находит хэш и по хэшу находим бакет
@@ -55,11 +57,54 @@ public class MyHashMap implements MyMap {
         return null;  // пара с ключом key не найдена
     }
 
-    // перебалансировка массива - создание массива в два раза больше и перенос туда всех элементов
+    // балансировка массива - создание массива в два раза больше и перенос туда всех элементов
     private void resize() {
         // нужно создать новый массив в два раза больше, чем source
-        // пробежаться по всем элементам
+        Pair[] newSource = new Pair[source.length * 2];
+        for (int i = 0; i < source.length; i++) {
+            Pair currentPair = source[i];
+            while (currentPair != null) {
+                // находим бакет в новом массиве
+                int bucket = Math.abs(currentPair.key.hashCode()) % newSource.length;
+                Pair newPair = newSource[bucket];
+                if (newPair == null) {
+                    newSource[bucket] = currentPair;
+                    currentPair = currentPair.next;
+                    continue;
+                } else {
+                    while (newPair != null) {
+                        newPair = newPair.next;
+                    }
+                    // в этом месте вместо newPair должно быть что-то связанное с newSource
+                    newPair = currentPair;
+                }
+                currentPair = currentPair.next;
+            }
+        }
+        source = newSource;
+    }
 
+//    Pair pair = findPair(key); // поиск пары по ключу
+//        if (pair == null) {
+//        int bucket = findBucket(key);  // поиск номера ведра по ключу
+//        pair = new Pair(key, value, source[bucket]);
+//        source[bucket] = pair;         // делаем новую пару корнем цепочки
+//        size++;
+//    } else {
+//        pair.value = value;            // такая пара уже есть, поэтому меняем ее значение
+//    }
+
+    // поиск пары по ключу для resize
+    private Pair findPairResize(String key, Pair[] newSource) {
+        int bucket = Math.abs(key.hashCode()) % newSource.length;
+        Pair currentPair = newSource[bucket];  // корень цепочки
+        while (currentPair != null) {
+            if (currentPair.key.equals(key)) {
+                return currentPair;
+            }
+            currentPair = currentPair.next; // следующая пара по цепочке
+        }
+        return null;  // пара с ключом key не найдена
     }
 
     @Override
