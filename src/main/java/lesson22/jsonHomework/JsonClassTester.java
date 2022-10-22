fb44c7c45229559dffbef57eaff9a1053d64e1fc
mikor85 <cormakoff@gmail.com>
2022-10-22 20:55:15 +0300

    Homework22 has been added

--- src/main/java/lesson22/jsonHomework/JsonClassTester.java
+++ src/main/java/lesson22/jsonHomework/JsonClassTester.java
@@ -14,13 +14,9 @@ public class JsonClassTester {
         Cat cat3 = new Cat("Gav", 1, "Red", "Siamese");
         String fileTxt = "jsonObject.txt";
         //jsonTxtFileWriter(cat1, fileTxt);
-        //System.out.println(jsonTxtFileReader(fileTxt));
-        jsonTxtFileReader(fileTxt);
-
-//        Gson gson = new Gson();
-//        StringBuilder test = new StringBuilder("{\"name\":\"Murzik\",\"age\":2,\"color\":\"Black\",\"breed\":\"Siberian\"}");
-//        Object resObject = gson.fromJson(String.valueOf(test), Object.class);
-//        System.out.println(resObject);
+        System.out.println(jsonTxtFileReaderMy(fileTxt));
+        Cat c = jsonTxtFileReader(fileTxt, Cat.class);
+        System.out.println(c);
     }
 
     public static void jsonTxtFileWriter(Object o, String file) {
@@ -35,21 +31,32 @@ public class JsonClassTester {
         }
     }
 
-    public static void jsonTxtFileReader(String file) {
+    public static <T> T jsonTxtFileReader(String file, Class<T> tClass) {
         Gson gson = new Gson();
-        //StringBuilder json = new StringBuilder();
-        String s = "";
-
+        StringBuilder json = new StringBuilder();
         try (FileReader fileReader = new FileReader(file);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            s = bufferedReader.readLine();
-            //json.append(bufferedReader.lines());
-            //bufferedReader.lines().forEach(System.out::println);
+            bufferedReader.lines().forEach(
+                    json::append
+            );
         } catch (IOException e) {
             System.out.println("Exception: " + e.getMessage());
         }
+        return gson.fromJson(json.toString(), tClass);
+    }
 
-        Object resObject = gson.fromJson(String.valueOf(s), Object.class);
-        System.out.println(resObject);
+    // My
+    public static Object jsonTxtFileReaderMy(String file) {
+        Gson gson = new Gson();
+        StringBuilder json = new StringBuilder();
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            bufferedReader.lines().forEach(
+                    json::append
+            );
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+        return gson.fromJson(json.toString(), Object.class);
     }
 }
