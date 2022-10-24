84a612c2be6e0a0b707e3f3b193eb26ea39e6f6b
mikor85 <cormakoff@gmail.com>
2022-10-24 20:47:13 +0300

    Homework23 has been added

--- src/main/java/lesson23/homework/AverageFromIntCollector.java
+++ src/main/java/lesson23/homework/AverageFromIntCollector.java
@@ -3,51 +3,60 @@ package lesson23.homework;
 // ДЗ проф ява:
 // 1. * Напишите коллектор который из потока целых вычисляет Double среднее значение.
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.Set;
-import java.util.concurrent.atomic.DoubleAccumulator;
-import java.util.function.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class AverageFromIntCollector {
     public static void main(String[] args) {
-        int[] ints = new int[]{0, 1, 2, 3, 4, 5, 6};
-        List<Integer> integerList = List.of(0, 1, 2, 3, 4, 5, 6);
-        System.out.println("average value of list1 = " + getIntsAverageStream(integerList));
-        List<Integer> integerList2 = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        System.out.println("average value of list2 = " + getIntsAverageStream(integerList2));
-        List<Integer> resultList = List.of(0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        System.out.println("average value of resultList = " + getIntsAverageStream(resultList));
-
-        double average1 = getIntsAverageStream(integerList);
-        double average2 = getIntsAverageStream(integerList2);
-        System.out.println((average1 + average2) / 2);
-
+        List<Integer> intList1 = List.of(0, 1, 2, 3, 4, 5, 6);
+        System.out.println("average value of list1 = " + getIntsAverageStream(intList1));
+        List<Integer> intList2 = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("average value of list2 = " + getIntsAverageStream(intList2));
+        System.out.println("average value of list1 = " + getIntsAverageCollector(intList1));
     }
 
     public static double getIntsAverageCollector(List<Integer> integerList) {
         return integerList.stream()
-                .mapToInt(i -> i)
-                .collect(new Supplier<Double>() {
-                             @Override
-                             public Double get() {
-                               return null;
-                             }
-                         },
-                        new ObjIntConsumer<Double>() {
-                            @Override
-                            public void accept(Double res, int value) {
+                .collect(new Collector<Integer, List<Integer>, Double>() {
+                    @Override
+                    public Supplier<List<Integer>> supplier() {
+                        return ArrayList::new;
+                    }
 
-                            }
-                        },
-                        new BiConsumer<Double, Double>() {
-                            @Override
-                            public void accept(Double d1, Double d2) {
+                    @Override
+                    public BiConsumer<List<Integer>, Integer> accumulator() {
+                        return List::add;
+                    }
 
-                            }
-                        });
+                    @Override
+                    public BinaryOperator<List<Integer>> combiner() {
+                        return (ints1, ints2) -> {
+                            ints1.addAll(ints2);
+                            return ints1;
+                        };
+                    }
+
+                    @Override
+                    public Function<List<Integer>, Double> finisher() {
+                        return integers -> {
+                            OptionalDouble res =
+                                    integers.stream().mapToDouble(i -> i).average();
+                            return res.isPresent() ? res.getAsDouble() : null;
+                        };
+                    }
+
+                    @Override
+                    public Set<Characteristics> characteristics() {
+                        return Set.of(Characteristics.UNORDERED);
+                    }
+                });
     }
 
     public static double getIntsAverageStream(List<Integer> integerList) {
