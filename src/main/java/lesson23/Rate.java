bcb61bde8c2adfa23056bc92428963cc0f3dc843
mikor85 <cormakoff@gmail.com>
2022-11-17 23:00:27 +0200

    lesson23 - Rate class has been changed.

--- src/main/java/lesson23/Rate.java
+++ src/main/java/lesson23/Rate.java
@@ -1,7 +1,13 @@
 package lesson23;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rate {
     public double amount;
     public String base;
     public String date;
+
+    public Map<String, Double> rates = new HashMap<>();
+    // {"amount":10.0,"base":"GBP","date":"2022-11-17","rates":{"USD":11.7965}}
 }
